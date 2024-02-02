cf45b430585323b996a1a7d5bb53a30472e51bb2
brandonbax <brandonbax2003@gmail.com>
2024-02-02 13:22:22 +0000

    namedWindow doesn't return anything. Just use string.

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import depthProcessing
 import map_visualisation
 import cv2 as cv
 
-dispMapWindow = cv.namedWindow("Disparity Map") #Disparity Map window
-downsampleMapWindow = cv.namedWindow("Downsampled Disparity Map")
+cv.namedWindow("Disparity Map") #Disparity Map window
+cv.namedWindow("Downsampled Disparity Map")
 
 #Real time loop
 while True:
     #Create and display disparity map
     dispMap = depthProcessing.produceDisparityMap(img1, img2)
-    map_visualisation.display_disparity(dispMap, dispMapWindow)
+    map_visualisation.display_disparity(dispMap, "Disparity Map")
 
     #Downsample disparity map and display
     downsampledMap = map_visualisation.downsample_map(dispMap)
-    map_visualisation.display_disparity(downsampledMap, downsampleMapWindow)
+    map_visualisation.display_disparity(downsampledMap, "Downsampled Disparity Map")
 
     #Produce depth for motor input
     depthMap = depthProcessing.produceDepthMap(downsampledMap, 1, 1)
