c812bafb502c07f4075f4deec124d7c7e561005c
Tristan Crocker <tristcrocker@gmail.com>
2024-02-02 12:12:17 +0000

    Add basic real time implementation without image input or motor output

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("Hello world")
 #Real time implementation in here
+import depthProcessing
+import map_visualisation
+import cv2 as cv
+
+dispMapWindow = cv.namedWindow("Disparity Map") #Disparity Map window
+downsampleMapWindow = cv.namedWindow("Downsampled Disparity Map")
+
+#Real time loop
+while True:
+    #Create and display disparity map
+    dispMap = depthProcessing.produceDisparityMap(img1, img2)
+    map_visualisation.display_disparity(dispMap, dispMapWindow)
+
+    #Downsample disparity map and display
+    downsampledMap = map_visualisation.downsample_map(dispMap)
+    map_visualisation.display_disparity(downsampledMap, downsampleMapWindow)
+
+    #Produce depth for motor input
+    depthMap = depthProcessing.produceDepthMap(downsampledMap, 1, 1)
