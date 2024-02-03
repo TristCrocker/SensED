f669cb05a30551d003651123c0ba43da48d538c9
Brandon <brandonbax2003@gmail.com>
2024-02-03 14:31:27 +0000

    fixes

--- map_visualisation.py
+++ map_visualisation.py
@@ -6,7 +6,7 @@ from skimage.measure import block_reduce
 def display_disparity(disp, window_name, colour=False):
     # Disparity map contains extreme values that cannot be displayed
     # By normalising the disparity map, it becomes possible to display it
-    normalised_image = cv2.normalize(disp, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX,
+    normalised_image = cv2.normalize(disp, disp, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX,
                                      dtype=cv2.CV_8U)
 
     # If colour is set to True then the output image will be in colour (rather than greyscale)
@@ -16,7 +16,7 @@ def display_disparity(disp, window_name, colour=False):
     # Displays the disparity map in the specified window
     cv2.imshow(window_name, normalised_image)
     # Waits 1ms for a key to be pressed, then stores the key that is pressed
-    key = cv2.waitKey(0)
+    key = cv2.waitKey(1)
     # If the pressed key is q then the program quits
     if key == ord('q'):
         quit()
