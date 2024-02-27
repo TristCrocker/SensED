811c0b6eb7d086dc7c91ef8490baa7a429fef083
SchorchingPassion <litmantas@gmail.com>
2024-02-27 11:44:22 +0000

    Updated the callibration array to better match motor position, added function that can be called for just a single sleave

--- motor_output.py
+++ motor_output.py
@@ -32,14 +32,14 @@ class PCA9685_Controller:
 
         # An dictionary for calibrating motor locations with physical locations (2 Tuples (phys_x, phys_y) : (controller_address, motor_channel))
         self.cal = {
-        (0,0) : (0x40, 0), (0,1) : (0x40, 1), (0,2) : (0x40, 2), (0,3) : (0x40, 3), (0,4) : (0x40, 4), (0,5) : (0x40, 5), (0,6) : (0x40, 6), (0,7) : (0x40, 7), 
-        (1,0) : (0x40, 8), (1,1) : (0x40, 9), (1,2) : (0x40, 10), (1,3) : (0x40, 11), (1,4) : (0x40, 12), (1,5) : (0x40, 13), (1,6) : (0x40, 14), (1,7) : (0x40, 15), 
-        (2,0) : (0x41, 0), (2,1) : (0x41, 1), (2,2) : (0x41, 2), (2,3) : (0x41, 3), (2,4) : (0x41, 4), (2,5) : (0x41, 5), (2,6) : (0x41, 6), (2,7) : (0x41, 7), 
-        (3,0) : (0x41, 8), (3,1) : (0x41, 9), (3,2) : (0x41, 10), (3,3) : (0x41, 11), (3,4) : (0x41, 12), (3,5) : (0x41, 13), (3,6) : (0x41, 14), (3,7) : (0x41, 15), 
-        (4,0) : (0x42, 0), (4,1) : (0x42, 1), (4,2) : (0x42, 2), (4,3) : (0x42, 3), (4,4) : (0x42, 4), (4,5) : (0x42, 5), (4,6) : (0x42, 6), (4,7) : (0x42, 7), 
-        (5,0) : (0x42, 8), (5,1) : (0x42, 9), (5,2) : (0x42, 10), (5,3) : (0x42, 11), (5,4) : (0x42, 12), (5,5) : (0x42, 13), (5,6) : (0x42, 14), (5,7) : (0x42, 15), 
-        (6,0) : (0x43, 0), (6,1) : (0x43, 1), (6,2) : (0x43, 2), (6,3) : (0x43, 3), (6,4) : (0x43, 4), (6,5) : (0x43, 5), (6,6) : (0x43, 6), (6,7) : (0x43, 7), 
-        (7,0) : (0x43, 8), (7,1) : (0x43, 9), (7,2) : (0x43, 10), (7,3) : (0x43, 11), (7,4) : (0x43, 12), (7,5) : (0x43, 13), (7,6) : (0x43, 14), (7,7) : (0x43, 15), 
+        (0,0) : (0x40, 0), (0,1) : (0x40, 8), (0,2) : (0x41, 0), (0,3) : (0x41, 8), (0,4) : (0x42, 0), (0,5) : (0x42, 8), (0,6) : (0x43, 0), (0,7) : (0x43, 8), 
+        (1,0) : (0x40, 1), (1,1) : (0x40, 9), (1,2) : (0x41, 1), (1,3) : (0x41, 9), (1,4) : (0x42, 1), (1,5) : (0x42, 9), (1,6) : (0x43, 1), (1,7) : (0x43, 9), 
+        (2,0) : (0x40, 2), (2,1) : (0x40, 10), (2,2) : (0x41, 2), (2,3) : (0x41, 10), (2,4) : (0x42, 2), (2,5) : (0x42, 10), (2,6) : (0x43, 2), (2,7) : (0x43, 10), 
+        (3,0) : (0x40, 3), (3,1) : (0x40, 11), (3,2) : (0x41, 3), (3,3) : (0x41, 11), (3,4) : (0x42, 3), (3,5) : (0x42, 11), (3,6) : (0x43, 3), (3,7) : (0x43, 11), 
+        (4,0) : (0x40, 4), (4,1) : (0x40, 12), (4,2) : (0x41, 4), (4,3) : (0x41, 12), (4,4) : (0x42, 4), (4,5) : (0x42, 12), (4,6) : (0x43, 4), (4,7) : (0x43, 12), 
+        (5,0) : (0x40, 5), (5,1) : (0x40, 13), (5,2) : (0x41, 5), (5,3) : (0x41, 13), (5,4) : (0x42, 5), (5,5) : (0x42, 13), (5,6) : (0x43, 5), (5,7) : (0x43, 13), 
+        (6,0) : (0x40, 6), (6,1) : (0x40, 14), (6,2) : (0x41, 6), (6,3) : (0x41, 14), (6,4) : (0x42, 6), (6,5) : (0x42, 14), (6,6) : (0x43, 6), (6,7) : (0x43, 14), 
+        (7,0) : (0x40, 7), (7,1) : (0x40, 15), (7,2) : (0x41, 7), (7,3) : (0x41, 15), (7,4) : (0x42, 7), (7,5) : (0x42, 15), (7,6) : (0x43, 7), (7,7) : (0x43, 15), 
         }
 
         self.i2c = busio.I2C(board.SCL, board.SDA)  # Create the I2C bus
@@ -72,6 +72,8 @@ class PCA9685_Controller:
 
         def motor_worker(controller_id, motor_data):
             """Worker function to control a single motor"""
+            if (controller_id[0] not in self.pca9685_instances):
+                return
             controller = self.pca9685_instances[controller_id[0]]
             channel = controller_id[1]  # Adjust this if using a different channel
             distance, pattern_id = motor_data
@@ -103,4 +105,33 @@ class PCA9685_Controller:
                 self.processes.append(p)
                 p.start()
 
+    def singleSleaveControl(self, intensity_pattern_array, shielMode=False):
+        '''Merges the array into a 8x4 by taking the max of the pairs of arrays in rows Also takes the largest pattern ID as the pattern'''
+        '''Shield mode (as suggested by Tim) takes the original array and transposes it so that the 8 motors display the image horizontally rather than vertically
+        If the user holds their arm like a shield it should display in accordance to that'''
         
+        if (shielMode):
+            intensity_pattern_array = np.transpose(intensity_pattern_array)
+
+        # Separate the intensity values from the pattern IDs
+        intensities = intensity_pattern_array[:, :, 0]
+
+        # Reshape the intensities array into four 2x8 subarrays
+        subarrays = intensities.reshape(4, 2, 8)
+
+        # Calculate the average intensity for each subarray
+        averages = np.max(subarrays, axis=1)
+        merged_array = averages.reshape(8, 4)
+
+        # Replace the intensity values in the merged array with tuples
+        for row in range(8):
+            for col in range(4):
+                if (intensity_pattern_array[row, col, 1] > intensity_pattern_array[row, col+1, 1]):
+                    pattern = intensity_pattern_array[row, col, 1]
+                else:
+                    pattern = intensity_pattern_array[row, col+1, 1]
+
+                merged_array[row, col] = (merged_array[row, col], pattern)
+
+        
+        self.control_motors(merged_array)
